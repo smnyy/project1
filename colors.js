4354c476b3939db88ae878ea2b8c0c2e1cc5d910
smnyy <68556909+smnyy@users.noreply.github.com>
2020-07-24 00:30:11 +0900

    4rd version

---
+++ colors.js
@@ -0,0 +1,39 @@
+var Links ={
+  setColor:function(color){
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    // alist[i].style.color = color;
+    // i = i + 1;
+    // }
+    $('a').css('color', color);
+  }
+}
+var Body = {
+  setColor:function (color){
+   //  document.querySelector('body').style.color=color;
+    $('body').css('color',color);
+  },
+  setBackgroundColor:function (color){
+    // document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor', color);
+  }
+}
+
+function darklighthandler(self){
+var target = document.querySelector('body')
+if (self.value === 'dark') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'light';
+
+    Links.setColor('darkred');
+
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'dark';
+
+    Links.setColor('red');
+    }
+  }
